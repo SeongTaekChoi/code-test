8d01ed621da79fbef3265a1d4c1393e4a0740b7b
SeongTaekChoi <165356685+SeongTaekChoi@users.noreply.github.com>
2026-07-14 20:43:21 +0900

    Time: 0 ms (100%), Space: 19.3 MB (20.56%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -6,15 +6,15 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-
         # node가 없는 경우
         if root == None:
             return None
-        # 재귀 함수 이용
+        # 노드에서 left와 right 교환
         temp = root.left
         root.left = root.right
         root.right = temp
 
+        # 재귀함수를 통해 모든 노드에 대하여 교환 수행
         self.invertTree(root.left)
         self.invertTree(root.right)
 
