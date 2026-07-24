abdebd7b832bfe18b63a6bb38a72a858048ffd9d
SeongTaekChoi <165356685+SeongTaekChoi@users.noreply.github.com>
2026-07-24 19:43:56 +0900

    Time: 7 ms (48.04%), Space: 20.4 MB (55.67%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,31 +1,27 @@
-from typing import List
-import collections
-
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # node_all은 사용하지 않아도 되므로 생략했습니다.
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
         node_ex = [i for i in range(len(isConnected))]
         count = 0
         
         # node_ex가 다 사라질 때까지 반복
         while len(node_ex) > 0:
-            # 1. 탐색을 시작할 노드를 하나 뽑아 큐에 넣고, node_ex에서 제거(방문 처리)
+            # 1. 탐색을 시작할 노드를 뽑고 리스트(큐 역할)에 삽입
             start_node = node_ex[0]
-            queue = collections.deque([start_node])
+            queue = [start_node] 
             node_ex.remove(start_node)
             
-            # 2. 노드 하나 뽑아서 연결관계 다 나올때까지 BFS
+            # 2. 큐가 빌 때까지 연결관계 탐색
             while queue:
-                curr = queue.popleft()
+                # 기본 리스트의 pop(0)을 사용하여 맨 앞의 원소를 꺼냄 (BFS 방식)
+                curr = queue.pop(0) 
                 
-                # 현재 노드(curr)와 모든 노드의 연결 상태를 확인
                 for next_node in range(len(isConnected)):
-                    # 두 노드가 연결되어 있고(1), 아직 탐색하지 않은 노드(node_ex에 존재)라면
+                    # 연결되어 있고 아직 방문하지 않은(node_ex에 있는) 노드라면
                     if isConnected[curr][next_node] == 1 and next_node in node_ex:
                         queue.append(next_node)
-                        node_ex.remove(next_node) # 큐에 넣음과 동시에 node_ex에서 빼서 중복 탐색 방지
+                        node_ex.remove(next_node)
             
-            # 연결된 녀석들을 다 찾아서 node_ex에서 뺐다면, 무리(Province) 하나를 찾은 것이므로 카운트 증가
+            # 연결된 한 무리를 다 찾았으므로 카운트 증가
             count += 1
             
         return count
